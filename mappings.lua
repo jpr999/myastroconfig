fa8dfe639c6d16c2d0eff81ff48c8ef3453ccfd8
Paul Rawcliffe <jpr999@gmail.com>
2023-06-19 07:41:57 +0100

    Changes to be committed: 	modified:   mappings.lua 	modified:   plugins/community.lua
    Added some mappings (not working)
    Enabled copilot

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,9 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
@@ -19,7 +21,10 @@ return {
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
+    -- CMake
+    ["<leader>Vg"] = { ":CMake configure<CR>", desc = "CMake configure" },
+    ["<leader>V"] = { Name = "CMake" },
   },
   t = {
     -- setting a mapping to false will disable it
